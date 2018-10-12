6bafbea45301f62d16deb09c3e53ed75f03a77bc
TheCodeCrafter <noahcoder77@gmail.com>
2018-10-12 14:38:47 +0000

    Added library and initialized file

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import objectdraw.*;
+
+
+public class Main extends WindowController {
+    
+    static final long serialVersionUID = 1;
+    
+    public static void main(String[] args) {
+	    System.out.println("Starting Controller...");
+	    new Main().startController(800, 600);
+    }
+    
+    public void begin() {
+        
+    }
+    
+    public void onMousePress(Location point) {
+        
+    }
+    
+    public void onMouseRelease(Location point) {
+        
+    }
+    
+    public void onMouseDrag(Location point) {
+        
+    }
+    
+    public void onMouseMove(Location point) {
+        
+    }
+    
+    public void onMouseEnter(Location point) {
+        
+    }
+    
+    public void onMouseExit(Location point) {
+        
+    }
+}
